a38b0d960cea595d2ca2cca8fe8fcfc48d033dca
Kayan-Patel <145233987+Kayan-Patel@users.noreply.github.com>
2023-11-18 17:29:17 -0600

    Add files via upload

--- Code/Red_Alliance_Front.java
+++ Code/Red_Alliance_Front.java
@@ -1,32 +1,3 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -54,9 +25,9 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
-@Autonomous(name="Blue_Alliance_Rear", group="Robot")
+@Autonomous(name="Red_Alliance_Front", group="Robot")
 //@Disabled
-public class BasicParking_Autonomous extends LinearOpMode {
+public class Red_Alliance_Front extends LinearOpMode {
 
     /* Declare OpMode members. */
 
@@ -94,26 +65,17 @@ public class BasicParking_Autonomous extends LinearOpMode {
 
         runtime.reset();
 
-        // Step 1:  Drive forward for 0.8 seconds
+
+
+
+
+        // Step 4:  Strafe right for 1.5 seconds
         FrontLeft.setPower(-0.5);
         BackLeft.setPower(-0.5);
-        FrontRight.setPower(-0.5);
-        BackRight.setPower(-0.5);
+        FrontRight.setPower(0.5);
+        BackRight.setPower(0.5);
         runtime.reset();
-        while (opModeIsActive() && (runtime.seconds() < 1.85)) {
-            telemetry.addData("Path", "Leg 2: %4.1f S Elapsed", runtime.seconds());
-            telemetry.update();
-        }
-
-
-
-        // Step 4:  Strafe left for 1.5 seconds
-        FrontLeft.setPower(+0.5);
-        BackLeft.setPower(+0.5);
-        FrontRight.setPower(-0.5);
-        BackRight.setPower(-0.5);
-        runtime.reset();
-        while (opModeIsActive() && (runtime.seconds() < 4.3)) {
+        while (opModeIsActive() && (runtime.seconds() < 2.3)) {
             telemetry.addData("Path", "Leg 3: %4.1f S Elapsed", runtime.seconds());
             telemetry.update();
         }
