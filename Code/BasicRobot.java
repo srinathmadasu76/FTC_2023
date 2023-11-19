8ce09609a2d5c77c2542ee0b6abe37e23f96e084
srinathmadasu76 <44925864+srinathmadasu76@users.noreply.github.com>
2023-11-18 21:13:25 -0600

    Add files via upload

--- Code/BasicRobot.java
+++ Code/BasicRobot.java
@@ -4,6 +4,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="teleop_robot")
 public class BasicRobot extends OpMode
@@ -12,6 +14,21 @@ public class BasicRobot extends OpMode
     DcMotor FrontRight = null;
     DcMotor BackLeft = null;
     DcMotor BackRight = null;
+
+    Servo leftClaw= null;
+    Servo rightClaw= null;
+    DcMotor arm= null;
+    double griprPosition = .2;
+    double griplPosition = .2;
+    double griprDelta = .1;
+    double griplDelta = .1;
+
+    private DcMotor armLeft;
+    private DcMotor armRight;
+
+    double          clawOffset  = 0.3 ;                  // Servo mid position
+    final double    CLAW_SPEED  = 0.01 ;                 // sets rate to move servo
+
     @Override
     public void init()
     {
@@ -24,11 +41,25 @@ public class BasicRobot extends OpMode
 
         FrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         BackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        leftClaw = hardwareMap.servo.get("leftclaw");
+        rightClaw = hardwareMap.servo.get("rightclaw");
+        arm = hardwareMap.dcMotor.get("arm");
+
+        //arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        //arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     @Override
     public void loop()
     {
+        if (gamepad2.left_stick_y!=0.0) {
+            arm.setPower(gamepad2.left_stick_y);
+        }
+        else{
+            arm.setPower(0.);
+        }
         if (gamepad1.left_stick_y!=0.0){
             FrontLeft.setPower(-gamepad1.left_stick_y);
             BackLeft.setPower(-gamepad1.left_stick_y);
@@ -57,6 +88,20 @@ public class BasicRobot extends OpMode
             FrontRight.setPower(-1);
             BackRight.setPower(1);
         }
+
+            if (gamepad2.y){
+            clawOffset += CLAW_SPEED;
+
+        }
+        else if (gamepad2.a){
+            clawOffset -= CLAW_SPEED;
+
+        }
+
+        // Move both servos to new position.  Assume servos are mirror image of each other.
+        clawOffset = Range.clip(clawOffset, -0.3, 0.3);
+        leftClaw.setPosition(clawOffset);
+        rightClaw.setPosition(-clawOffset);
 
     }
 }
